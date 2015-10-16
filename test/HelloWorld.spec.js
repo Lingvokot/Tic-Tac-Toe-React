86c9bba327048a87d7664b3909ba2232fa9361ee
ColCh <colchgh@gmail.com>
2015-10-17 02:06:25 +0300

    Initial spec file with jsdom

--- test/HelloWorld.spec.js
+++ test/HelloWorld.spec.js
@@ -1,1 +1,14 @@
 // component test
+import jsdom from "mocha-jsdom";
+import assert from "assert";
+
+import HelloWorld from "../src/components/HelloWorld.jsx";
+
+describe("HelloWorld component", () => {
+
+  jsdom();
+
+  it("should exist", () => {
+    assert.ok( !!HelloWorld );
+  });
+});
