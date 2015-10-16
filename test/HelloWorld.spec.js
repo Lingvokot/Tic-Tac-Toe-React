5b0f8fc6a3f00104b3b0bc7d3ad1e70d3a544e1c
ColCh <colchgh@gmail.com>
2015-10-17 02:26:21 +0300

    Add DOM test case

--- test/HelloWorld.spec.js
+++ test/HelloWorld.spec.js
@@ -2,6 +2,7 @@
 import jsdom from "mocha-jsdom";
 import assert from "assert";
 import React from "react";
+import ReactDOM from "react-dom";
 import ReactTestUtils from "react-addons-test-utils";
 
 import HelloWorld from "../src/components/HelloWorld.jsx";
@@ -17,6 +18,16 @@ describe("HelloWorld component", () => {
   it("should be react element", () => {
     assert.ok( ReactTestUtils.isElement(<HelloWorld />),
       "is not a react element" );
+  });
+
+  describe("rendering", () => {
+
+    it("should display 'text' prop in DOM somehow", () => {
+      const container = document.createElement("div");
+      ReactDOM.render(<HelloWorld text="TEST" />, container);
+      assert.ok( /TEST/.test(container.innerHTML), "It don't displays 'text' prop in DOM");
+    });
+
   });
 
 });
