196d6adbc6d426dbe440b69dc20e1014202d639e
Max Petruck <petruck.maxim@gmail.com>
2015-10-23 15:28:52 +0300

    Add X and O images and React image component to place them inside the cell

--- src/components/GameScreen/GameGrid.jsx
+++ src/components/GameScreen/GameGrid.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+const imageX = "images/x.png";
+const imageO = "images/o.png";
+
 const GameGrid = React.createClass({
   gameGridStyle: {
     display: "flex",
@@ -23,17 +26,53 @@ const GameGrid = React.createClass({
 });
 
 const GridCell = React.createClass({
+  getInitialState: function () {
+    return {
+      cellValue: "none"
+    }
+  },
+
   gridCellStyle: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     width: "9.7rem",
     height: "9.7rem",
     backgroundColor: "#6f71a0"
   },
 
+  renderImage: function() {
+    switch(this.state.cellValue) {
+      case "x":
+        return <Image image={imageX} />;
+      case "o":
+        return <Image image={imageO} />
+      default:
+        return;
+    }
+  },
+
   render () {
     return (
       <div style={this.gridCellStyle}>
-
+        {this.renderImage()}
       </div>
+    );
+  }
+});
+
+const Image = React.createClass({
+  PropTypes: {
+    image: React.PropTypes.string
+  },
+
+  imageStyle: {
+    width: "95%"
+  },
+
+  render () {
+    return (
+      <img style={this.imageStyle} src={this.props.image}></img>
     );
   }
 });
