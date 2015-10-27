ba559f2a7289c4f3478f0a1cc645f13d8a390606
Max Petruck <petruck.maxim@gmail.com>
2015-10-27 13:08:42 +0200

    Add tests for MenuScreen component

---
+++ test/components/MenuScreen/MenuScreen.spec.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactTestUtils from "react-addons-test-utils";
+
+import MenuScreen from "../../../src/components/MenuScreen/MenuScreen.js";
+
+describe("MenuScreen", () => {
+
+  it("should exist", () => {
+    MenuScreen.should.exist;
+  });
+
+  it("should be react element", () => {
+    ReactTestUtils.isElement(<MenuScreen />).should.be.ok;
+  });
+
+});
