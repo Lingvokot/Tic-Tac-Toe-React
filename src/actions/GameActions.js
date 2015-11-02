3a0036a8f61c6a97147ad5edb14e0ead6729c348
Max Petruck <petruck.maxim@gmail.com>
2015-11-02 14:27:21 +0200

    Add Game Actions

---
+++ src/actions/GameActions.js
@@ -0,0 +1,10 @@
+// Game action types
+export const GRID_CHANGED = "GRID_CHANGED";
+
+// GRID_CHANGED action creator
+export function changeGridAction(x, y) {
+  return {
+    x: x,
+    y: y, type:
+    GRID_CHANGED};
+}
