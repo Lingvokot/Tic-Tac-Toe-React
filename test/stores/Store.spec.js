1c6cfedaca2be22f1720b4f6cd64764199be173e
Max Petruck <petruck.maxim@gmail.com>
2015-11-03 11:32:59 +0200

    Refactor: Add test if store use app reducer

--- test/stores/Store.spec.js
+++ test/stores/Store.spec.js
@@ -1,4 +1,5 @@
 import store from "src/stores/Store.js";
+import app from "src/reducers/App.js";
 
 describe("store", () => {
 
@@ -12,5 +13,11 @@ describe("store", () => {
     store.replaceReducer.should.be.a.function;
     store.subscribe.should.be.a.function;
   });
+
+  it("should use app reducer", () => {
+    var storeState = store.dispath({type: "bla-bla-bla"});
+    var defaultState = app(undefined, {type: "bla-bla-bla"});
+    storeState.should.be.eql(defaultState);
+  })
 
 });
