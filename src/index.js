30e78d321187ca718d47c769696137a6646a68da
Max Petruck <petruck.maxim@gmail.com>
2015-11-03 15:13:12 +0200

    Refactor: code clean up, remove all excess code

--- src/index.js
+++ src/index.js
@@ -4,29 +4,8 @@ import { Provider } from "react-redux";
 import store from "./stores/Store.js";
 import App from "./components/App.jsx";
 
-import setCurrentScreenAction from "./actions/ScreenActions.js";
-import { MENU_SCREEN, GAME_SCREEN } from "./actions/ScreenActions.js";
-import changeGridAction from "./actions/GameActions.js";
-
-// this function handles all button clicks (for now)
-function buttonClickHandler(text) {
-  switch(text) {
-    case "Menu":
-      store.dispatch(setCurrentScreenAction(MENU_SCREEN))
-      break;
-    default:
-      store.dispatch(setCurrentScreenAction(GAME_SCREEN))
-  }
-}
-
-function cellClickHandler(x, y) {
-  store.dispatch(changeGridAction(x,y));
-}
-
 ReactDOM.render(
   <Provider store={store}>
-    <App buttonClickHandler={buttonClickHandler}
-        cellClickHandler={cellClickHandler}
-    />
+    <App/>
   </Provider>,
   document.getElementById("app"));
