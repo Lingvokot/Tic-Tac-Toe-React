bea4f8f5b4a4075c88bc567e7f56d95cd1a0ca6e
Max Petruck <petruck.maxim@gmail.com>
2015-11-04 12:17:54 +0200

    Change button props
    Now button onClick handler is anonymous function

--- src/components/MenuScreen/MenuScreen.js
+++ src/components/MenuScreen/MenuScreen.js
@@ -3,11 +3,14 @@ import React from "react";
 import Container from "./Container.js";
 import Button from "./../Shared/Button.js";
 
+import setCurrentScreenAction from "../../actions/ScreenActions.js";
+import { GAME_SCREEN } from "../../actions/ScreenActions.js";
+
 const screenTitle = "Tic Tac Toe React";
 
 const MenuScreen = React.createClass({
   PropTypes: {
-    buttonClickHandler: React.PropTypes.func.isRequired
+    dispatch: React.PropTypes.func.isRequired
   },
 
   headerStyle: {
@@ -21,22 +24,24 @@ const MenuScreen = React.createClass({
         <h1 style={this.headerStyle}>
           {screenTitle}
         </h1>
-        <Button buttonClickHandler={this.props.buttonClickHandler}
+        <Button onClick={() => this.props.dispatch(
+                setCurrentScreenAction(GAME_SCREEN)
+              )}
             text="Play with Human"
             useWrapper
         />
         <h2 style={this.headerStyle}>
           {"Play with computer"}
         </h2>
-        <Button buttonClickHandler={this.props.buttonClickHandler}
+        <Button buttonClickHandler={()=>1}
             text="EASY"
             useWrapper
         />
-        <Button buttonClickHandler={this.props.buttonClickHandler}
+        <Button buttonClickHandler={()=>1}
             text="MEDIUM"
             useWrapper
         />
-        <Button buttonClickHandler={this.props.buttonClickHandler} 
+        <Button buttonClickHandler={()=>1}
             text="HARD"
             useWrapper
         />
