2a1046019d7ae33ef37468e9d2369e69691b05f5
Max Petruck <petruck.maxim@gmail.com>
2015-11-09 15:56:49 +0200

    Add props rendering test

--- test/components/GameScreen/GameStats.spec.js
+++ test/components/GameScreen/GameStats.spec.js
@@ -13,4 +13,25 @@ describe("GameStats component", () => {
     ReactTestUtils.isElement(<GameStats />).should.be.ok;
   });
 
+  describe("rendering", () => {
+
+    it("should display statistics in DOM", () => {
+      const statistics = {
+        x: 5,
+        o: 123456
+      };
+      const renderer = ReactTestUtils.createRenderer();
+      renderer.render(<GameStats victoryStatistics={statistics} />);
+      const output = renderer.getRenderOutput();
+
+      output.props.children[1]
+            .props.children[1]
+            .props.children.should.be.equal(statistics.x);
+      output.props.children[2]
+            .props.children[1]
+            .props.children.should.be.equal(statistics.o);
+    });
+
+  });
+
 });
