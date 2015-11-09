a927e3b5ec64ef7041123f66e85912c453a68b99
Max Petruck <petruck.maxim@gmail.com>
2015-11-09 15:32:51 +0200

    Add new actions
    - Rename old GRID_CHANGED action into PLAYER_MOVE
    - Add COMPUTER_MOVE action and action creator for it
    - Add RESET_GAME action and action creator for it
    - Add SET_GAME_MODE action and action creator for it

--- src/actions/GameActions.js
+++ src/actions/GameActions.js
@@ -1,13 +1,33 @@
 // Game action types
-export const GRID_CHANGED = "GRID_CHANGED";
+export const PLAYER_MOVE = "PLAYER_MOVE";
+export const COMPUTER_MOVE = "COMPUTER_MOVE";
+export const RESET_GAME = "RESET_GAME";
+export const SET_GAME_MODE = "SET_GAME_MODE";
 
 // GRID_CHANGED action creator
-const changeGridAction = function (x, y) {
+export const playerMoveAction = function (x, y) {
   return {
     x: x,
     y: y,
-    type: GRID_CHANGED
+    type: PLAYER_MOVE
   };
-}
+};
 
-export default changeGridAction;
+export const computerMoveAction = function () {
+  return {
+    type: COMPUTER_MOVE
+  };
+};
+
+export const resetGameAction = function () {
+  return {
+    type: RESET_GAME
+  };
+};
+
+export const setGameModeAction = function (mode) {
+  return {
+    mode: mode,
+    type: SET_GAME_MODE
+  };
+};
