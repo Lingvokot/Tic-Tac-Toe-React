2a3ef2cbfe30266aa7c3ad846c93bbe2aee75d57
Max Petruck <petruck.maxim@gmail.com>
2015-11-15 09:44:50 +0200

    Major changes
    - Remove PLAYER_MOVE and COMPUTER_MOVE, now there’s only APPLY_MOVE action and action creator
    - Add applyComputerMoveAfterTimeout function to make computer move async
    - Add START_NEXT_MATCH action
    - Add shouldHumanMove and shouldComputerMove methods
    - Add gameTick function to manage all in game actions based on current state

--- src/actions/GameActions.js
+++ src/actions/GameActions.js
@@ -1,25 +1,29 @@
+import { VS_HUMAN } from "../reducers/Game.js";
+import computeMove from "./AI.js";
+
 // Game action types
-export const PLAYER_MOVE = "PLAYER_MOVE";
-export const COMPUTER_MOVE = "COMPUTER_MOVE";
+export const APPLY_MOVE = "PLAYER_MOVE";
 export const RESET_GAME = "RESET_GAME";
 export const SET_GAME_MODE = "SET_GAME_MODE";
+export const START_NEXT_MATCH = "START_NEXT_MATCH";
 
 const MOVE_TIMEOUT = 200;
 
 // GRID_CHANGED action creator
-export const playerMoveAction = function (x, y) {
+export const applyMoveAction = function ({x, y}) {
   return {
     x: x,
     y: y,
-    type: PLAYER_MOVE
+    type: APPLY_MOVE
   };
 };
 
-export const computerMoveAction = function () {
-  return {
-    type: COMPUTER_MOVE
-  };
-};
+export const applyComputerMoveAfterTimeout = function (state, timeout) {
+  return dispatch => setTimeout(
+      () => dispatch(applyMoveAction(computeMove(state))),
+      timeout
+    );
+}
 
 export const resetGameAction = function () {
   return {
@@ -34,10 +38,41 @@ export const setGameModeAction = function (mode) {
   };
 };
 
-export const computerMoveActionAsync = function () {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(computerMoveAction(), MOVE_TIMEOUT);
-    });
+export const startNextMacthAction = function () {
+  return {
+    type: START_NEXT_MATCH
   }
-};
+}
+
+const shouldHumanMove = function (state, {x, y}) {
+  return state.gameGrid[x][y] === "" &&
+    (state.gameMode === VS_HUMAN ||
+     ((state.currentTurn === "x") !== state.AI.playingX)
+    );
+}
+
+const shouldComputerMove = function (state) {
+  return state.gameMode !== VS_HUMAN &&
+    (state.currentTurn === "x") === state.AI.playingX;
+}
+
+const gameTick = function (move) {
+  return (dispatch, getState) => {
+    let state = getState().game;
+    if(move) {
+      if(state.gameOver) {
+        dispatch(startNextMacthAction());
+        return;
+      }
+      if(shouldHumanMove(state, move)) {
+        dispatch(applyMoveAction(move));
+      }
+      return;
+    }
+    if(shouldComputerMove(state) && !state.gameOver) {
+      dispatch(applyComputerMoveAfterTimeout(state, MOVE_TIMEOUT));
+    }
+  }
+}
+
+export default gameTick;
