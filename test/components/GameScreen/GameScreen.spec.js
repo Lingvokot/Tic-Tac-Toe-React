ae99df10b265cee939c6e1cd982d63dce0776912
Max Petruck <petruck.maxim@gmail.com>
2015-11-16 18:49:40 +0200

    Add tests for menu button, cell click handlers and component update handler
    tests check if they cause dispatch calls

--- test/components/GameScreen/GameScreen.spec.js
+++ test/components/GameScreen/GameScreen.spec.js
@@ -1,10 +1,19 @@
 // component test
 import React from "react";
 import ReactTestUtils from "react-addons-test-utils";
+import jsdom from "mocha-jsdom";
 
 import GameScreen from "src/components/GameScreen/GameScreen.js";
+import Button from "src/components/Shared/Button.js";
+import GameGrid, { GridCell } from "src/components/GameScreen/GameGrid.js";
+
+import setCurrentScreenAction from "src/actions/ScreenActions.js";
+import { MENU_SCREEN } from "src/actions/ScreenActions.js";
+import { resetGameAction } from "src/actions/GameActions.js";
 
 describe("GameScreen", () => {
+
+  jsdom();
 
   it("should exist", () => {
     GameScreen.should.exist;
@@ -12,6 +21,75 @@ describe("GameScreen", () => {
 
   it("should be react element", () => {
     ReactTestUtils.isElement(<GameScreen />).should.be.ok;
+  });
+
+  describe("Buttons", () => {
+
+    let actions = [],
+        dispatch = (action) => actions.push(action);
+
+    it("Button should dispatch proper actions on click", () => {
+      const gameScreen = ReactTestUtils.renderIntoDocument(
+        <GameScreen dispatch={dispatch}
+            cellValues={[["","",""],["","",""],["","",""]]}
+            victoryStatistics={{x:0,y:0}}/>
+      );
+      var button = ReactTestUtils.findRenderedComponentWithType(
+          gameScreen,
+          Button
+        );
+      var node = ReactTestUtils
+          .findRenderedDOMComponentWithTag(button, "button");
+
+      ReactTestUtils.Simulate.click(node);
+
+      actions[0].should.be.eql(setCurrentScreenAction(MENU_SCREEN));
+      actions[1].should.be.eql(resetGameAction());
+      actions = [];
+    });
+
+  });
+
+  describe("Grid cells", () => {
+
+    let actions = [],
+        dispatch = (action) => actions.push(action);
+
+    it("Should use cellClickHandler", () => {
+      const gameScreen = ReactTestUtils.renderIntoDocument(
+        <GameScreen dispatch={dispatch}
+            cellValues={[["","",""],["","",""],["","",""]]}
+            victoryStatistics={{x:0,y:0}}/>
+      );
+
+      ReactTestUtils.scryRenderedComponentsWithType(
+        gameScreen,
+        GridCell
+      ).forEach( gridCell => {
+        var node = ReactTestUtils
+            .findRenderedDOMComponentWithTag(gridCell, "div");
+        ReactTestUtils.Simulate.click(node);
+        actions[0].should.be.function;
+        actions = [];
+      });
+    });
+
+    it("Should use onUpdate", () => {
+      const gameScreen = ReactTestUtils.renderIntoDocument(
+        <GameScreen dispatch={dispatch}
+            cellValues={[["","",""],["","",""],["","",""]]}
+            victoryStatistics={{x:0,y:0}}/>
+      );
+
+      var grid = ReactTestUtils.findRenderedComponentWithType(
+        gameScreen,
+        GameGrid
+      );
+
+      grid.forceUpdate();
+      actions[0].should.be.function;
+    });
+
   });
 
 });
