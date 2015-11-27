10a2d78e18f0608af252b398e1fb78d07ec1c7e9
Max Petruck <petruck.maxim@gmail.com>
2015-11-27 17:04:36 +0200

    Refactor: transform Button component to ES6 class, and ButtonWrapper functional component
    Updated props validation and moved inline style, to this module scope due to ES6 class component specifics
    
    Transform ButtonWrapper component to functional component

--- src/components/Shared/Button.js
+++ src/components/Shared/Button.js
@@ -1,38 +1,28 @@
 import React from "react";
 
-const Button = React.createClass({
-  PropTypes: {
-    text: React.PropTypes.string,
-    useWrapper: React.PropTypes.string,
-    onClick: React.PropTypes.func.isRequired
-  },
+class Button extends React.Component {
 
-  buttonStyle: {
-    fontSize: "1.5rem",
-    boxShadow: "0.2rem 0.2rem 0.2rem rgba(0,0,0,0.8)"
-  },
-
-  buttonWithWrapper: function () {
+  buttonWithWrapper () {
     return (
       <ButtonWrapper>
         <button onClick={this.props.onClick}
-            style={this.buttonStyle}
+            style={buttonStyle}
         >
           {this.props.text}
         </button>
       </ButtonWrapper>
     );
-  },
+  }
 
-  buttonWithoutWrapper: function () {
+  buttonWithoutWrapper () {
     return (
       <button onClick={this.props.onClick}
-          style={this.buttonStyle}
+          style={buttonStyle}
       >
         {this.props.text}
       </button>
     );
-  },
+  }
 
   render () {
     return (
@@ -41,20 +31,29 @@ const Button = React.createClass({
         this.buttonWithoutWrapper()
       );
   }
-});
+}
 
-const ButtonWrapper = React.createClass({
-  wrapperStyle: {
+Button.propTypes = {
+  onClick: React.PropTypes.func.isRequired,
+  text: React.PropTypes.string,
+  useWrapper: React.PropTypes.string
+};
+
+let buttonStyle = {
+  fontSize: "1.5rem",
+  boxShadow: "0.2rem 0.2rem 0.2rem rgba(0,0,0,0.8)"
+};
+
+const ButtonWrapper = (props) => {
+  let wrapperStyle = {
     margin: "1rem auto"
-  },
-
-  render () {
-    return (
-      <div style={this.wrapperStyle}>
-        {this.props.children}
-      </div>
-    );
   }
-});
+
+  return (
+    <div style={wrapperStyle}>
+      {props.children}
+    </div>
+  );
+}
 
 export default Button;
