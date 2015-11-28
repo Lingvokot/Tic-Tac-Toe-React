cf7fb5e69ead3fe09e99664da6bafe5f80298e10
Max Petruck <petruck.maxim@gmail.com>
2015-11-28 11:16:21 +0200

    Refactor: transform GameGrid and GameCell to ES6 class
    Remove OnClick function and use anonymous function for click event handling

--- src/components/GameScreen/GameGrid.js
+++ src/components/GameScreen/GameGrid.js
@@ -3,34 +3,18 @@ import React from "react";
 export const imageX = "images/x.png";
 export const imageO = "images/o.png";
 
-const GameGrid = React.createClass({
+class GameGrid extends React.Component {
   componentDidUpdate() {
     this.props.onUpdate();
-  },
-
-  PropTypes: {
-    cellClickHandler: React.PropTypes.func.isRequired,
-    cellValues: React.PropTypes.array.isRequired,
-    onUpdate: React.PropTypes.func
-  },
-
-  gameGridStyle: {
-    display: "flex",
-    flexFlow: "row wrap",
-    justifyContent: "space-between",
-    alignContent: "space-between",
-    width: "30rem",
-    height: "30rem",
-    backgroundColor: "#123456"
-  },
+  }
 
   renderGrid(grid, clickHandler) {
     var gridToRender = [];
     for(let i = 0; i < grid.length; i++) {
       for(let j = 0; j < grid[i].length; j++) {
         gridToRender.push(
-          <GridCell clickHandler={clickHandler}
-              cellValue={grid[i][j]}
+          <GridCell cellValue={grid[i][j]}
+              clickHandler={clickHandler}
               key={i+""+j}
               x={i}
               y={j}
@@ -39,35 +23,36 @@ const GameGrid = React.createClass({
       }
     }
     return gridToRender;
-  },
+  }
 
-  render () {
+  render() {
     return (
-      <div style={this.gameGridStyle}>
+      <div style={gameGridStyle}>
         {this.renderGrid(this.props.cellValues, this.props.cellClickHandler)}
       </div>
     );
   }
-});
+}
 
-export const GridCell = React.createClass({
-  PropTypes: {
-    clickHandler: React.PropTypes.func.isRequired,
-    cellValue: React.PropTypes.string,
-    x: React.PropTypes.number,
-    y: React.PropTypes.number
-  },
+GameGrid.propTypes = {
+  cellClickHandler: React.PropTypes.func,
+  cellValues: React.PropTypes.array,
+  onUpdate: React.PropTypes.func
+};
 
-  gridCellStyle: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "9.7rem",
-    height: "9.7rem",
-    backgroundColor: "#6f71a0"
-  },
+let gameGridStyle = {
+  display: "flex",
+  flexFlow: "row wrap",
+  justifyContent: "space-between",
+  alignContent: "space-between",
+  width: "30rem",
+  height: "30rem",
+  backgroundColor: "#123456"
+};
 
-  renderImage: function(value) {
+class GridCell extends React.Component {
+
+  renderImage(value) {
     switch(value) {
       case "x":
         return <Image image={imageX} />;
@@ -76,22 +61,35 @@ export const GridCell = React.createClass({
       default:
         return;
     }
-  },
+  }
 
-  onClick() {
-     this.props.clickHandler(this.props.x, this.props.y);
-  },
-
-  render () {
+  render() {
     return (
-      <div onClick={this.onClick}
-          style={this.gridCellStyle}
+      <div onClick={() => this.props.clickHandler(this.props.x, this.props.y)}
+          style={gridCellStyle}
       >
         {this.renderImage(this.props.cellValue)}
       </div>
     );
   }
-});
+}
+
+GridCell.propTypes = {
+  cellValue: React.PropTypes.string,
+  clickHandler: React.PropTypes.func.isRequired,
+  x: React.PropTypes.number,
+  y: React.PropTypes.number
+};
+
+
+let gridCellStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "9.7rem",
+  height: "9.7rem",
+  backgroundColor: "#6f71a0"
+};
 
 const Image = (props) => {
   var imageStyle = {
